dbb271e4950e549c52498186d3997782d6f9bbc9
jinwook song <wlsdnr129@naver.com>
2024-11-30 22:55:35 +0900

    refactor: server

--- server/src/server/server.rs
+++ server/src/server/server.rs
@@ -1,4 +1,7 @@
-use std::{io::Read, net::TcpListener};
+use std::io::Read;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+
+use crate::http::Request;
 
 pub struct Server {
     addr: String,
@@ -13,23 +16,41 @@ impl Server {
     // Instance Method
     pub fn run(self) {
         println!("✅ Listen on {}", self.addr);
-        let listener = TcpListener::bind(&self.addr).unwrap();
+        let listener = TcpListener::bind(&self.addr).unwrap_or_else(|e| {
+            panic!("Failed to bind to address {}: {}", self.addr, e)
+        });
 
         loop {
             match listener.accept() {
-                Ok((mut stream, _)) => {
-                    // Array: 사용하기전에 초기화
-                    let mut buffer = [0; 1024];
-
-                    match stream.read(&mut buffer) {
-                        Ok(_) => {
-                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
-                        }
-                        Err(e) => println!("Failed to read from connection: {}", e),
-                    }
+                Ok((stream, addr)) => {
+                    println!("🔗 Accepted connection from {}", addr);
+                    handle_accept(stream, addr);
                 }
-                Err(e) => println!("Failed to establish a connection: {}", e),
+                Err(e) => println!("❌ Failed to accept connection: {}", e),
             }
+        }
+    }
+}
+
+/// Handles a single client connection.
+fn handle_accept(mut stream: TcpStream, _: SocketAddr) {
+    // Initialize a buffer for reading data
+    let mut buffer = [0; 1024];
+
+    match stream.read(&mut buffer) {
+        Ok(_) => {
+            println!(
+                "Received a request: {}",
+                String::from_utf8_lossy(&buffer)
+            );
+
+            match Request::try_from(&buffer[..]) {
+                Ok(request) => todo!(),
+                Err(e) => println!("❌ Failed to parse request: {}", e),
+            }
+        }
+        Err(e) => {
+            println!("Failed to read from connection: {}", e)
         }
     }
 }
