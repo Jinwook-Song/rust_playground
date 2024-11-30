ab10ec2d188732a8e527d5715a3be529ff9f2742
jinwook song <wlsdnr129@naver.com>
2024-11-30 21:58:10 +0900

    feat: accept request & convert utf8 to string

--- server/src/server/server.rs
+++ server/src/server/server.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{io::Read, net::TcpListener};
 
 pub struct Server {
     addr: String,
@@ -17,8 +17,18 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, addr)) => todo!(),
-                Err(e) => println!("Failed to connection: {}", e),
+                Ok((mut stream, _)) => {
+                    // Array: 사용하기전에 초기화
+                    let mut buffer = [0; 1024];
+
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
             }
         }
     }
