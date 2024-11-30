90866fc4e9dc79bb904a80323d0015afeee490e4
jinwook song <wlsdnr129@naver.com>
2024-11-30 18:49:35 +0900

    feat: enum & option

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
+    let get = Method::GET;
+    let post = Method::POST;
+    let put = Method::PUT;
+    let delete = Method::DELETE;
+
     let addr = String::from("127.0.0.1:8080");
     let server = Server::new(addr);
     server.run();
@@ -18,4 +23,22 @@ impl Server {
     fn run(self) {
         println!("✅ Listen on {}", self.addr)
     }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    HEAD,
+    TRACE,
+    OPTIONS,
+    CONNECT,
+    PATCH,
 }
