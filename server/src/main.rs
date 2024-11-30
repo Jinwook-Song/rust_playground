d648fc328f638abe6e8ff777b317c45eff72337a
jinwook song <wlsdnr129@naver.com>
2024-11-30 18:22:21 +0900

    feat: String, string slice, Struct, impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let addr = String::from("127.0.0.1:8080");
+    let server = Server::new(addr);
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    // Static Method (Associated Function)
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    // Instance Method
+    fn run(self) {
+        println!("✅ Listen on {}", self.addr)
+    }
 }
