fa8ade4082cbe2285658e2ce6c07bf96f31d092f
jinwook song <wlsdnr129@naver.com>
2024-11-30 23:07:20 +0900

    feat: trait & error handling

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,6 +1,71 @@
+use core::str;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str::Utf8Error;
+
 use super::method::Method;
+
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        // match str::from_utf8(buf) {
+        //     Ok(_) => {}
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        // }
+
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+        //     Ok(_) => todo!(),
+        //     Err(e) => return Err(e),
+        // }
+
+        let request = str::from_utf8(buf)?;
+
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            ParseError::InvalidRequest => "Invalid request",
+            ParseError::InvalidEncoding => "Invalid encoding",
+            ParseError::InvalidProtocol => "Invalid protocol",
+            ParseError::InvalidMethod => "Invalid method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
