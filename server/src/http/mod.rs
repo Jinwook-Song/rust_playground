209487a68329fc7bfdf5c2b2e8374f75d7d97e91
jinwook song <wlsdnr129@naver.com>
2024-12-01 18:00:54 +0900

    feat: response struct

--- server/src/http/mod.rs
+++ server/src/http/mod.rs
@@ -1,8 +1,10 @@
 mod method;
 mod query_string;
 mod request;
+mod response;
 
 pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValule};
 pub use request::ParseError;
 pub use request::Request;
+pub use response::Response;
