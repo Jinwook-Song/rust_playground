37978349c5428f382e40b98c7a285bbf1188573a
jinwook song <wlsdnr129@naver.com>
2024-12-01 18:15:12 +0900

    feat(coyp, clone): status code

--- server/src/http/mod.rs
+++ server/src/http/mod.rs
@@ -2,9 +2,11 @@ mod method;
 mod query_string;
 mod request;
 mod response;
+mod status_code;
 
 pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValule};
 pub use request::ParseError;
 pub use request::Request;
 pub use response::Response;
+pub use status_code::StatusCode;
