94e0153a291dd9bb6852e003e3457b6ba07ec9f8
jinwook song <wlsdnr129@naver.com>
2024-12-01 20:18:44 +0900

    feat: routing

--- server/src/website_handler/website_handler.rs
+++ server/src/website_handler/website_handler.rs
@@ -1,10 +1,29 @@
-use super::super::http::{Request, Response, StatusCode};
+use std::fs;
+
+use super::super::http::{Method, Request, Response, StatusCode};
 use super::super::server::Handler;
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
-    fn handle_request(&mut self, _: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => {
+                    let html = fs::read_to_string("docs/index.html")
+                        .unwrap_or_else(|_| {
+                            "<h1>Failed to load page</h1>".to_string()
+                        });
+                    Response::new(StatusCode::Ok, Some(html))
+                }
+                "/about" => Response::new(
+                    StatusCode::Ok,
+                    Some("About the website".to_string()),
+                ),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
